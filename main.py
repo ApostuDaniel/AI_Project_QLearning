2aa9d31d30246f8eb199715c510453276412d229
ApostuDaniel <apostudanbs@gmail.com>
2023-01-23 17:44:29 +0200

    Added RL-supervized and supervized-RL algorithm mix

--- main.py
+++ main.py
@@ -15,14 +15,18 @@ from DeepQ_Agent import Agent
 from IrisGym import IrisGym
 
 
-def reinforcement_learning(variables_train, target_train, variables_test, target_test):
+def reinforcement_learning(variables_train, target_train, variables_test, target_test, episodes, network_weights=None):
     env = IrisGym(dataset=(variables_train, target_train))
     lr = 0.001
-    n_games = 1500
+    n_games = episodes
     agent = Agent(gamma=0.01, epsilon=1.0, lr=lr,
                   input_dims=env.observation_space.shape,
                   n_actions=3, mem_size=100000, batch_size=1,
                   epsilon_end=0.01)
+    if network_weights is not None:
+        #to initialize the weights we need to call predict once
+        agent.q_eval.predict(np.asarray(variables_train[0]).reshape((1, len(variables_train[0]))))
+        agent.q_eval.set_weights(network_weights)
     scores = []
     eps_history = []
 
@@ -46,12 +50,13 @@ def reinforcement_learning(variables_train, target_train, variables_test, target
               'average_score %.2f' % avg_score,
               'epsilon %.2f' % agent.epsilon)
 
+    return agent.q_eval
     # filename = 'lunarlander_tf2.png'
     # x = [i + 1 for i in range(n_games)]
     # plotLearning(x, scores, eps_history, filename)
 
 
-def supervized_learning(variables_train, target_train, variables_test, target_test):
+def supervized_learning(variables_train, target_train, variables_test, target_test, epochs, network_weights=None):
     model = Sequential([
         Dense(50, activation='relu', input_dim=4),
         Dense(40, activation='relu'),
@@ -61,9 +66,11 @@ def supervized_learning(variables_train, target_train, variables_test, target_te
     model.compile(optimizer='adam',
                   loss='mean_squared_error',
                   metrics=['accuracy'])
-
+    if network_weights is not None:
+        model.predict(np.asarray(variables_train[0]).reshape((1, len(variables_train[0]))))
+        model.set_weights(network_weights)
     # build the model
-    model.fit(variables_train, target_train, epochs=50, verbose=0)
+    model.fit(variables_train, target_train, epochs=epochs, verbose=1)
 
     pred_train = model.predict(variables_train)
     scores = model.evaluate(variables_train, target_train, verbose=0)
@@ -72,7 +79,21 @@ def supervized_learning(variables_train, target_train, variables_test, target_te
     pred_test = model.predict(variables_test)
     scores2 = model.evaluate(variables_test, target_test, verbose=0)
     print('Accuracy on test data: {}% \n Error on test data: {}'.format(scores2[1], 1 - scores2[1]))
+    return model
 
+
+def supervized_to_RL(variables_train, target_train, variables_test, target_test, epochs, episodes):
+    y_train = to_categorical(target_train)
+    y_test = to_categorical(target_test)
+    trained_network = supervized_learning(variables_train, y_train, variables_test, y_test, epochs)
+    reinforcement_learning(variables_train, target_train, variables_test, target_test, episodes,
+                           trained_network.get_weights())
+
+def RL_to_supervized(variables_train, target_train, variables_test, target_test, epochs, episodes):
+    y_train = to_categorical(target_train)
+    y_test = to_categorical(target_test)
+    trained_network = reinforcement_learning(variables_train, target_train, variables_test, target_test, episodes, None)
+    supervized_model = supervized_learning(variables_train, y_train, variables_test, y_test, epochs, trained_network.get_weights())
 
 if __name__ == '__main__':
     all_data = pd.read_csv(r'resources/iris.data', header=0,
@@ -84,10 +105,13 @@ if __name__ == '__main__':
     y = all_data[target].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
 
-    #pentru reinforcement learning decomentati linia asta
-    reinforcement_learning(X_train, y_train, X_test, y_test)
+    # supervized_to_RL(X_train, y_train, X_test, y_test, 50, 750)
+    RL_to_supervized(X_train, y_train, X_test, y_test, 50, 750)
+
+    # pentru reinforcement learning decomentati linia asta
+    # reinforcement_learning(X_train, y_train, X_test, y_test, 1500)
 
     # pentru learning cu reteaua neuronala decomentatie urmatoarele linii
     # y_train = to_categorical(y_train)
     # y_test = to_categorical(y_test)
-    # supervized_learning(X_train, y_train, X_test, y_test)
+    # supervized_learning(X_train, y_train, X_test, y_test, 50)
